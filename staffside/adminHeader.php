bad20530f296ab510de77e3c0ea0cf1556ca7317
paulsskiii <95641543+paulsskiii@users.noreply.github.com>
2023-12-04 13:36:45 +0800

    staffside v2

--- staffside/adminHeader.php
+++ staffside/adminHeader.php
@@ -16,13 +16,13 @@
         <?php           
         if(isset($_SESSION['user_id'])){
           ?>
-          <a href="../logout.php" style="text-decoration:none;">
+          <a href="logout.php" style="text-decoration:none;">
             <i class="fa fa-user mr-5" style="font-size:30px; color:#fff;" aria-hidden="true"></i>
          </a>
           <?php
         } else {
             ?>
-            <a href="../logout.php" style="text-decoration:none;">
+            <a href="logout.php" style="text-decoration:none;">
                     <i class="fa fa-sign-in mr-5" style="font-size:30px; color:#fff;" aria-hidden="true"></i>
             </a>
 
