690aa763bf8cf7355a440b88f0dfc832fc30d838
paulsskiii <95641543+paulsskiii@users.noreply.github.com>
2023-12-04 17:20:29 +0800

    staffside v2

--- staffside/config.php
+++ staffside/config.php
@@ -1,5 +1,6 @@
 <?php
 
-$conn = mysqli_connect('localhost','root','','roqs');
+$conn = mysqli_connect('localhost','root','','swiss_collection');
+// old connection roqs data to swiss_collection
 
 ?>
