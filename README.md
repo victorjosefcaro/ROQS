a992c431d9538f9df851d2e8e89311fdaf088e40
Victor Caro <victorjosefcaro@gmail.com>
2025-05-06 14:01:51 +0800

    Initial commit

--- README.md
+++ README.md
@@ -1,7 +1,1 @@
-Members
-
-Paul Baesa
-Pierce Gervin Parce
-Raphael Nepomuceno
-Victor Josef Caro
-Vincent Yuriel
+# ROQS
--- staffside/adminView/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$conn = mysqli_connect('localhost','root','','roqsmain');
-// old connection roqs data to swiss_collection
-
-// from swiss_collection to new database roqsmain 
-
-?>
